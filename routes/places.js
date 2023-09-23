4652a56a9213db96f8d339b651b142f9494652b0
Vanessa Cavaco Branco <vanessa.cavaco.branco@gmail.com>
2023-09-23 20:56:27 +0200

    endpoints put y delete of trips & endpoints of places get, push, put, delete

--- routes/places.js
+++ routes/places.js
@@ -2,9 +2,68 @@ var express = require('express');
 var router = express.Router();
 const db = require("../model/helper");
 
-/* GET Database listening */
-router.get('/', function(req, res, next) {
-  res.send('welcome to the API');
+/********* PLACES **********/
+/* GET ALL - todos los places de la BBDD*/
+router.get("/", (req, res) => {
+  // llama a la lista completa de places atraves de la funcion db
+  db("SELECT * FROM places ORDER BY id ASC;")
+      .then(results => {
+          res.send(results.data);
+      })
+      .catch(error => res.status(500).send(error));
+});
+
+/* POST - añade un nuevo place */
+router.post("/", async (req, res) => {
+  try {
+      //el body que contiene la data a subir
+      const body = req.body;
+      //si no hay error se inserta en la tabla la data del body
+      const sql = `INSERT INTO places (trip_id,name,coordinates,day,date,description) VALUES ('${body.trip_id}','${body.name}','${body.coordinates}','${body.day}','${body.date}','${body.description}');`; //actualizar parametros
+      //se adiciona a la tabla usando la funcion db y como parametros se da sql
+      await db(sql)
+      // Envía una respuesta de éxito con el código 201
+      res.sendStatus(201);
+  } catch (error) {
+      // Si ocurre un error, envía una respuesta de error con el código 500
+      console.error(error);
+      res.sendStatus(500);
+  };
+});
+
+/* PUT - edita un trip */
+router.put("/:place_id", async (req, res) => {
+  try {
+      ///el body que contiene la data a actualizar
+      const body = req.body;
+      // Los parámetros de la URL que están disponibles en req.params
+      const id = req.params.place_id;
+      //si no hay error se actualiza trips segun el body recibido
+      const sql = `UPDATE places SET name = '${body.name}', coordinates = '${body.coordinates}', day = '${body.day}', date = '${body.date}', description = '${body.description}' WHERE id = ${id};`;
+      await db(sql);
+      // Envía una respuesta de éxito con el código 201
+      res.sendStatus(201);
+  } catch (error) {
+      // Si ocurre un error, envía una respuesta de error con el código 500
+      console.error(error);
+      res.sendStatus(500);
+  };
+});
+
+/* DELETE - elimina una trip */
+router.delete("/:place_id", async (req, res) => {
+  try {
+      // Los parámetros de la URL están disponibles en req.params
+      const id = req.params.place_id;
+      const sql = `DELETE FROM places WHERE id = ${id};`;
+      await db(sql);
+      // Envía una respuesta de éxito con el código 201
+      res.sendStatus(201);
+  } catch (error) {
+      // Si ocurre un error, envía una respuesta de error con el código 500
+      console.error(error);
+      res.sendStatus(500);
+  };
 });
 
 
--- routes/trips.js
+++ routes/trips.js
@@ -4,32 +4,67 @@ const db = require("../model/helper");
 
 /********* TRIPS **********/
 /* GET ALL - todos los trips de la BBDD*/
-router.get("/",(req,res)=>{
+router.get("/", (req, res) => {
     // llama a la lista completa de trips atraves de la funcion db
     db("SELECT * FROM trips ORDER BY id ASC;")
-        .then(results =>{
+        .then(results => {
             res.send(results.data);
         })
         .catch(error => res.status(500).send(error));
-  });
+});
 
 /* POST - añade una nueva trip */
-router.post("/", async(req,res)=>{
-    try{
+router.post("/", async (req, res) => {
+    try {
         //el body que contiene la data a subir
         const body = req.body;
-        console.log("eso es el body de trips",body.user_id,body.name,body.coordinates,body.date,body.description);
         //si no hay error se inserta en la tabla la data del body
         const sql = `INSERT INTO trips (user_id,name,coordinates,date,description) VALUES ('${body.user_id}','${body.name}','${body.coordinates}','${body.date}','${body.description}');`; //actualizar parametros
-        //se usa el try and catch para volver a hacer un get de las trips actualizada con la trip adicionada.
+        //se adiciona a la tabla usando la funcion db y como parametros se da sql
         await db(sql)
         // Envía una respuesta de éxito con el código 201
-            res.sendStatus(201);
-    }catch (error) {
+        res.sendStatus(201);
+    } catch (error) {
         // Si ocurre un error, envía una respuesta de error con el código 500
         console.error(error);
         res.sendStatus(500);
     };
-    });
+});
+
+/* PUT - edita un trip */
+router.put("/:trip_id", async (req, res) => {
+    try {
+        ///el body que contiene la data a actualizar
+        const body = req.body;
+        // Los parámetros de la URL que están disponibles en req.params
+        const id = req.params.trip_id;
+        //si no hay error se actualiza trips segun el body recibido
+        const sql = `UPDATE trips SET name = '${body.name}', coordinates = '${body.coordinates}', date = '${body.date}', description = '${body.description}' WHERE id = ${id};`;
+        await db(sql);
+        // Envía una respuesta de éxito con el código 201
+        res.sendStatus(201);
+    } catch (error) {
+        // Si ocurre un error, envía una respuesta de error con el código 500
+        console.error(error);
+        res.sendStatus(500);
+    };
+});
+
+
+/* DELETE - elimina una trip */
+router.delete("/:trip_id", async (req, res) => {
+    try {
+        // Los parámetros de la URL están disponibles en req.params
+        const id = req.params.trip_id;
+        const sql = `DELETE FROM trips WHERE id = ${id};`;
+        await db(sql);
+        // Envía una respuesta de éxito con el código 201
+        res.sendStatus(201);
+    } catch (error) {
+        // Si ocurre un error, envía una respuesta de error con el código 500
+        console.error(error);
+        res.sendStatus(500);
+    };
+});
 
 module.exports = router;
