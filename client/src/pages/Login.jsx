8ccf129d4c3471bccd62069ed470a1463b93ce06
Vinay  Font <vinayfont@MacBook-Air-de-Vinay.local>
2023-09-23 17:18:21 +0200

    react-routes

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import "./Login.css"
+
+function Login() {
+    const [formData, setFormData] = useState({
+      username: '',
+      password: '',
+    });
+
+    return (
+      // dentro de <div hacer un formulario con username y password 
+      <div>Login</div>
+    ); 
+
+    };
+
+export default Login
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,5 @@ var router = express.Router();
 const db = require("../model/helper");
 
 /* GET Database listening */
-router.get('/', function(req, res, next) {
-  res.send({ 'welcome to the API' });
-});
 
 module.exports = router;
