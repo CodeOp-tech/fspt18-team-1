ed2e5db167ec115a8d6e58a908200914128f7701
Elena Manea <maneaelenasorina@gmail.com>
2023-09-27 18:03:52 +0200

    registration form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,44 @@
+import React, { useState, useEffect } from 'react';
 import './App.css'
 import NavBar from './componentes/NavBar';
 import Signup from './components/Signup';
 import Login from "./pages/Login"
 import MyTrip from "./pages/MyTrip"
 import Trips from "./pages/Trips"
+import RegistrationForm from './pages/RegistrationForm';
+import Logout from './components/Logout';
+
 import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate,
+
 } from "react-router-dom";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+
   return (
     <Router>
       <NavBar/>
       <Routes>
+      {isLoggedIn ? (
+          <Route path="/trips" element={<Trips />} />
+        ) : (
+          <Route path="/registration-form" element={<RegistrationForm />} />
+        )}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/logout" element={<Logout />} /> 
           <Route path="/trips" element={<Trips />} />
           <Route path="/mytrip" element={<MyTrip />} />
         </Routes>
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const HOSTNAME = "http://localhost:5000";
+
+function Login() {
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+      });
+
+    const { username, password } = credentials;
+    
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setCredentials({ ...credentials, [name]: value });
+      };
+
+      const login = async () => {
+        try {
+          const { data } = await axios(`${HOSTNAME}/users/login`, {
+            method: "POST",
+            data: credentials,
+          });
+          localStorage.setItem("token", data.token);
+          console.log(data.message, data.token);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      return(
+        <div>
+            <div>
+                <input
+                    value={username}
+                    onChange={handleChange}
+                    name="username"
+                    type="text"
+                    className=""
+                />
+                <input
+                    value={password}
+                    onChange={handleChange}
+                    name="password"
+                    type="password"
+                    className=""
+                />
+                <button className="" onClick={login}>
+                    Log in
+                </button>
+                <Link to="/signup">Registrarse</Link>
+                </div>
+        </div>
+      )
+    
+}
+export default Login;
