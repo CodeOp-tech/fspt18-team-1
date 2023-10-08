02533210cc2ddd0aff108e5d5a07701085028f2b
Elena Manea <maneaelenasorina@gmail.com>
2023-10-08 12:20:00 +0200

    props coordinates issues

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import React, { useState } from 'react';
+//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-function TravelProfile({ coordinates }) {
-  const position = [coordinates.lat, coordinates.lng];
-
+function Map(mapTrip) {
+  // const position = [mapTrip.latitude, mapTrip.longitude];
+  //const [trip, setTrip] = useState([props.mapTrip]);
+  console.log('Contenido de mapTrip:', mapTrip);
   return (
     <div>
-      <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
+      <div key={trip.id}>
+        <p> {trip.latitude} </p>
+      </div>
+      {/* <MapContainer center={position} zoom={13} style={{ height: '400px', width: '100%' }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
         <Marker position={position}>
           <Popup>
-            Luogo del viaggio
+              Viaje
           </Popup>
         </Marker>
-      </MapContainer>
+      </MapContainer> */}
     </div>
+
   );
 }
 
-export default TravelProfile;
+export default Map;
